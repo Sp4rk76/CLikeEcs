843317885f83f9348321f1a32eb5b167b8faca58
Dumas Valentin <valentin.dumas@viacesi.fr>
2017-11-12 20:54:20 +0100

    Vertices and colors tests

--- src/scenes/SceneOpenGL.cpp
+++ src/scenes/SceneOpenGL.cpp
@@ -2,6 +2,7 @@
 // Created by Sp4rk on 12-11-17.
 //
 
+#include <effects/Shader.h>
 #include "scenes/SceneOpenGL.h"
 
 SceneOpenGL::SceneOpenGL()
@@ -9,7 +10,7 @@ SceneOpenGL::SceneOpenGL()
     screen_ = new Screen();
 }
 
-SceneOpenGL::SceneOpenGL(const std::string& title, int width, int height, int fps)
+SceneOpenGL::SceneOpenGL(const std::string &title, int width, int height, int fps)
 {
     screen_ = new Screen(title, width, height, fps);
 }
@@ -60,8 +61,19 @@ void SceneOpenGL::simulate()
     // Variables (vertices, etc...
     bool quit(false);
 
-    float vertices[] = {0.0, 0.0,   0.5,0.0,   0.0,0.5,
-                        -0.8, -0.8,   -0.3, -0.8,   -0.8, -0.3};
+    float vertices[] = {-0.5, 0.0, 0.0, 1.0, 0.5, 0.0,
+                        -0.5, 0.0, 0.0, -1.0, 0.5, 0.0};
+    float colors[] = {240.0 / 255.0, 210.0 / 255.0, 23.0 / 255.0,
+                      230.0 / 255.0, 0.0, 230.0 / 255.0,
+                      0.0, 1.0, 0.0,
+
+                      240.0 / 255.0, 210.0 / 255.0, 23.0 / 255.0,
+                      0.0, 0.0, 1.0,
+                      0.0, 1.0, 0.0};
+
+    // Create a Shader
+    Shader shaderColor("shaders/couleur2D.vert", "shaders/couleur2D.frag");
+    shaderColor.charger();
 
     while (!quit)
     {
@@ -74,10 +86,26 @@ void SceneOpenGL::simulate()
 
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // Activate Shader
+        glUseProgram(shaderColor.getProgramID());
+
+        // Define / Send vertices
         glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, vertices);
         glEnableVertexAttribArray(0);
+
+        // Define / Send colors
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, colors);
+        glEnableVertexAttribArray(1);
+
+        // Display triangle with shader
         glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        /// Note: deactivate array in reverse activation order !
+        glDisableVertexAttribArray(1);
         glDisableVertexAttribArray(0);
+
+        // Deactivate Shader
+        glUseProgram(0);
 
         SDL_GL_SwapWindow(screen_->getWindow());
     }
